d44956b3ed855875fc479948d4697fd622633fe9
Gaji Yaqub <gajiyakub6@gmail.com>
2025-05-03 13:40:19 -0700

    Started the homepage of Kwik Speak

--- Main_Speaker/Main_Speaker/urls.py
+++ Main_Speaker/Main_Speaker/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # I included their url paths
+    path('lessons/', include('lessons.urls')),
+    path('quizzes/', include('quizzes.urls')),
+    path('users/', include('users.urls'))
 ]
